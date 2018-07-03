b96e6c18eccb4e064e30a08401f74af77fe3840e
Przemysław Sawczuk <przemeksawczuk@gmail.com>
2018-07-03 13:03:17 +0200

    Added creeps source allocation

--- main.js
+++ main.js
@@ -4,17 +4,18 @@ var roleBuilder = require('role.builder');
 
 module.exports = {
     data: {
+        'sourcesSpace': [2,6],
         'harvesterCount': 2,
         'harvesterSchemaV1': [WORK, WORK, CARRY, MOVE],
-        'harvesterSchemaV2': [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE],
+        'harvesterSchemaV2': [WORK, WORK, WORK, CARRY, MOVE, MOVE],
         'harvesterSchemaV3': [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE],
         'upgraderCount': 2,
         'upgraderSchemaV1': [WORK, WORK, CARRY, MOVE],
-        'upgraderSchemaV2': [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE],
+        'upgraderSchemaV2': [WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE],
         'upgraderSchemaV3': [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE],
         'builderCount': 6,
         'builderSchemaV1': [WORK, WORK, CARRY, MOVE],
-        'builderSchemaV2': [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE],
+        'builderSchemaV2': [WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE],
         'builderSchemaV3': [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE]
     },
     getData: function () {
@@ -22,30 +23,25 @@ module.exports = {
     },
     loop: function () {
         data = this.getData();
+        creepCounter = 0;
+        sourcesSpaceIndex = 0;
 
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
         var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-        var energySchemaLevel = null;
 
-        if (Game.spawns.Home.energy >= 550) {
-            energySchemaLevel = 'V3';
-        } else if (Game.spawns.Home.energy >= 450) {
-            energySchemaLevel = 'V2';
-        } else if (Game.spawns.Home.energy >= 300) {
-            energySchemaLevel = 'V1';
-        }
+        energySchemaLevel = this.schemaLevel(harvesters.length);
 
         if (energySchemaLevel !== null) {
             this.clearCreeps();
             if (harvesters.length < data.harvesterCount) {
-                Game.spawns.Home.createCreep(data['harvesterSchema'+energySchemaLevel], null, {role: 'harvester'});
+                Game.spawns.Home.createCreep(data['harvesterSchema' + energySchemaLevel], 'Harvester_' + Game.time, {role: 'harvester'});
             }
             else if (upgraders.length < data.upgraderCount) {
-                Game.spawns.Home.createCreep(data['upgraderSchema'+energySchemaLevel], null, {role: 'upgrader'});
+                Game.spawns.Home.createCreep(data['upgraderSchema' + energySchemaLevel], 'Upgrader_' + Game.time, {role: 'upgrader'});
             }
             else if (builders.length < data.builderCount) {
-                Game.spawns.Home.createCreep(data['builderSchema'+ energySchemaLevel], null, {role: 'builder'});
+                Game.spawns.Home.createCreep(data['builderSchema' + energySchemaLevel], 'Builder_' + Game.time, {role: 'builder'});
             }
         }
         if (Game.spawns.Home.spawning) {
@@ -60,13 +56,19 @@ module.exports = {
         for (var name in Game.creeps) {
             var creep = Game.creeps[name];
             if (creep.memory.role == 'harvester') {
-                roleHarvester.run(creep, 1);
+                roleHarvester.run(creep, sourcesSpaceIndex);
             }
             else if (creep.memory.role == 'upgrader') {
-                roleUpgrader.run(creep, 0);
+                roleUpgrader.run(creep, sourcesSpaceIndex);
             }
             else if (creep.memory.role == 'builder') {
-                roleBuilder.run(creep, 1);
+                roleBuilder.run(creep, sourcesSpaceIndex);
+            }
+
+            creepCounter++;
+            if (creepCounter >= data.sourcesSpace[sourcesSpaceIndex]) {
+                creepCounter = 0;
+                sourcesSpaceIndex = (sourcesSpaceIndex + 1) % data.sourcesSpace.length;
             }
         }
     },
@@ -77,6 +79,19 @@ module.exports = {
                 console.log('Clearing non-existing creep memory:', name);
             }
         }
+    },
+    schemaLevel: function (harvesterLength = 0) {
+        if (Game.spawns.Home.room.energyCapacityAvailable >= 550) {
+            if (Game.spawns.Home.room.energyAvailable >= 550)
+                return 'V3';
+        } else if (Game.spawns.Home.room.energyCapacityAvailable >= 450) {
+            if (Game.spawns.Home.room.energyAvailable >= 450)
+                return 'V2';
+        } else if (Game.spawns.Home.room.energyAvailable >= 300) {
+            return 'V1';
+        }
+        if (harvesterLength == 0 && Game.spawns.Home.room.energyAvailable >= 300)
+            return 'V1';
+        return null;
     }
 }
-
